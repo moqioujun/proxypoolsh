aad4d283ca6b1012177f7b539028b1f3815b9850
moqioujun <66248398+moqioujun@users.noreply.github.com>
2021-11-23 12:28:21 +0800

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -4,8 +4,8 @@ proxypool=$(ls ~/go/pkg/mod/github.com/'!sansui233')
 chmod -R 777 ~/go/pkg/mod/github.com/'!sansui233'
 cp -r ~/go/pkg/mod/github.com/'!sansui233'/$proxypool ~/proxypool
 rm -rf ~/go/pkg/mod/github.com/'!sansui233' ~/proxypool/config/config.yaml ~/proxypool/config/source.yaml
-wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/config.yaml
-wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/source.yaml
+wget -P ~/proxypool/config https://raw.githubusercontent.com/moqioujun/proxypoolsh/main/termux/config.yaml
+wget -P ~/proxypool/config https://raw.githubusercontent.com/moqioujun/proxypoolsh/main/termux/source.yaml
 wget -N "https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/proxypool.sh"
 chmod +x proxypool.sh
 rm install.sh
